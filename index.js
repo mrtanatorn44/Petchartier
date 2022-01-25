d3be8e8a5fb284ae05c144021d16c98daf8043f1
mrtanatorn44 <mrtanatorn44@gmail.com>
2022-01-25 22:23:22 +0700

    V.2.0.0 Add features

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const config = require('./config');
 const commands = require('./help');
 
 
-// Require dependencies
+// Crypto Currency
 const dotenv = require('dotenv');
 const axios = require('axios'); 	// New line that we added
 
-// COMMAND case uptime
+// Command : uptime, uptimenow
 let uptime_toggle = false;
 var count_uptime_hour = 0;
+
+// Command : timer
+let isTimer = false;
+let intervalTimer;
+
+function isInt(str) {
+  return !isNaN(str) && Number.isInteger(parseFloat(str));
+}
 
 let bot = new Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
@@ -23,59 +31,107 @@ let bot = new Client({
       type: 'LISTENING'
     }
   }
-  
-  
 });
 
-bot.on('ready', () => console.log(`Logged in as ${bot.user.tag}.`));
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}.`)
+
+  var time_now;
+  setInterval(function() {
+    time_now = new Date();
+    // bot.user.setActivity('Valorant', { type: 'Playing' });
+    bot.user.setActivity(time_now.toLocaleTimeString());
+  }, 5000);
+
+  /*
+  var d = new Date();
+  console.log(d.toLocaleTimeString());
+  console.log(d.toLocaleString());
+  console.log(d.toLocaleDateString());
+  */
+
+});
+
+  
 
 bot.on('message', async message => {
+
   // Check for command
   if (message.content.startsWith(config.prefix)) {
     let args = message.content.slice(config.prefix.length).split(' ');
     let command = args.shift().toLowerCase();
     let msg = "";
 
-    if (message.content.startsWith('!price')) {
-      // Get the params
-      const [command, ...args] = message.content.split(' ');
-  
-      // Check if there are two arguments present
-      if (args.length !== 2) {
-        return message.reply(
-          'You must provide the crypto and the currency to compare with!'
-        );
-      } else {
-        const [coin, vsCurrency] = args;
-        try {
-          // Get crypto price from coingecko API
-          const { data } = await axios.get(
-            `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${vsCurrency}`
-          );
-  
-          // Check if data exists
-          if (!data[coin][vsCurrency]) throw Error();
-  
-          return message.reply(
-            `The current price of 1 ${coin} = ${data[coin][vsCurrency]} ${vsCurrency}`
-          );
-        } catch (err) {
-          return message.reply(
-            'Please check your inputs. For example: !price bitcoin usd'
-          );
-        }
-      }
-    }
-
-    let uptime = false;
     switch (command) {
+      case 'timer':
+        if (args.length !== 1) {
+          message.reply(
+            '[Timer] Try again! -> !timer [minutes]'
+          );
+        } else {
+          if (args == 'off' && !isInt(args)) {
+            if (isTimer) {
+              message.channel.send({ content: '[Timer] Turn off!'});
+              isTimer = false;
+              clearInterval(intervalTimer);
+            } else {
+              message.channel.send({ content: '[Timer] Not set!'});
+            }
+          } else if (isInt(args)) {
+            if (isTimer) {
+              message.reply(
+                '[Timer] Is running!'
+              );
+            } else {
+              isTimer = true;
+              message.channel.send({ content: `[Timer] Countdown ${args} minutes start!`});
+              intervalTimer = setInterval(() => {
+                message.channel.send({ content: '[Timer] Time out!'});
+                clearInterval(intervalTimer);
+                isTimer = false;
+              }, args*60*1000);
+            }
+          } else {
+            message.reply(
+              '[Timer] Try again! -> !timer [integer]'
+            );
+          }
+        }
+
+        break;
+      case 'price':
+
+        if (args.length !== 2) {
+          message.reply(
+            'You must provide the crypto and the currency to compare with!'
+          );
+        } else {
+          const [coin, vsCurrency] = args;
+          try {
+            // Get crypto price from coingecko API
+            const { data } = await axios.get(
+              `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${vsCurrency}`
+            );
+            // Check if data exists
+            if (!data[coin][vsCurrency])
+              throw Error();
+
+            message.reply(
+              `The current price of 1 ${coin} = ${data[coin][vsCurrency]} ${vsCurrency}`
+            );
+          } catch (err) {
+            return message.reply(
+              'Please check your inputs. For example: !price bitcoin usd'
+            );
+          }
+        }
+        break;
 
       case 'porn':
         message.channel.send({ content: 'No horny! :point_left:', files: ['./images/nohorny.png'] });
         break;
         
       case 'uptimenow':
-        
         let totalSeconds = (bot.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
         totalSeconds %= 86400;
@@ -83,69 +139,41 @@ bot.on('message', async message => {
         totalSeconds %= 3600;
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = Math.floor(totalSeconds % 60);
-
         let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
         message.channel.send({ content: `[Uptime] ${uptime} :white_check_mark:`});
-
         break;
       
-      case 'uptime':
-
-        var checkminutes = 60, checkthe_interval = checkminutes * 60 * 1000; 
-        //This checks every 10 minutes, change 10 to whatever minute you'd like
-        
-        setInterval(function() {
-          count_uptime_hour += 1;
-          if (uptime_toggle) {
-            message.channel.send({ content: `[Uptime] ${count_uptime_hour} hours :white_check_mark:`});      
-          }
-        }, checkthe_interval);
-
-        if (uptime_toggle) {
-          uptime_toggle = false;
-          message.channel.send({ content: '[Uptime notification] OFF'});       
-        } else {
-          uptime_toggle = true;
-          message.channel.send({ content: '[Uptime notification] ON'});
-        }
-
-        break;
-
       case 'ping':
         msg = await message.reply('Pinging...');
         await msg.edit(`[PING] ${Date.now() - msg.createdTimestamp}ms.`)
         break;
       
       case 'tawan':
-        //msg = await message.reply('Sending...');
-        message.channel.send({ content: 'ตาหวานๆ :eyes:', files: ['./images/tawan.png'] });
+        message.channel.send({ 
+          content: 'ตาหวานๆ :eyes:', 
+          files: ['./images/tawan.png'] 
+        });
         break;
 
       case 'clearchat':
         if (message.member.permissions.has("ADMINISTRATOR")) {
-
           let fetched;
           let removed = 0;
 
           do {
-
             fetched = await message.channel.messages.fetch({limit: 100});
             if (fetched.size == 0)
               break;
-            //console.log("Fetch : " + fetched.size);
+            // console.log("Fetch : " + fetched.size);
             await message.channel.bulkDelete(fetched.size)
               .then(removed += fetched.size)
-              .catch(console.error);
-                 
+              .catch(console.error);       
           } while(fetched.size > 1);
 
           message.channel.send({ content: `Delete ${removed} messages` });
-
         } else {
-
           message.reply('You don\'t have permission!');
-
-        }
+        } 
         break;
 
       case 'say':
@@ -157,7 +185,6 @@ bot.on('message', async message => {
           message.reply('You did not send a message to repeat, cancelling command.')
         break
 
-      /* Unless you know what you're doing, don't change this command. */
       case 'help':
         let embed =  new MessageEmbed()
           .setTitle('HELP MENU')
@@ -175,6 +202,7 @@ bot.on('message', async message => {
 
             if (commands[command].aliases)
               embed.addField('Command aliases', `\`${commands[command].aliases.join('`, `')}\``);
+            
             embed
               .addField('DESCRIPTION', commands[command].description)
               .addField('FORMAT', `\`\`\`${config.prefix}${commands[command].format}\`\`\``);
@@ -186,6 +214,7 @@ bot.on('message', async message => {
         }
         message.channel.send(embed);
         break;
+
     }
   }
 });
